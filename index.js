2226f025d5dd6e69b622fdaf176d59d38e37333d
gabinacodes <badruraiah@gmail.com>
2021-12-21 13:49:13 +0100

    push6

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 document.getElementById("ham").onclick = function name(params) {
     document.getElementById("div-one").style.display = "block"
     document.getElementById("ham").style.display = "none"
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 document.getElementById("img").onclick = function name(params) {
     document.getElementById("div-one").style.display = "none"
     document.getElementById("ham").style.display = "block"
+   
 }
