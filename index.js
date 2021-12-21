4c5439662d1b4a10a630ba63cf890ab3cf76ad02
gabinacodes <badruraiah@gmail.com>
2021-12-21 12:25:50 +0100

    push

---
+++ index.js
@@ -0,0 +1,9 @@
+ document.getElementById("div-one").style.display = "none"
+document.getElementById("ham").onclick = function name(params) {
+    document.getElementById("div-one").style.display = "block"
+}
+document.getElementById("img").onclick = function name(params) {
+    document.getElementById("div-one").style.display = "none"
+}
+
+console.log("divee")
